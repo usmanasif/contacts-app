2921ba4b00225c990a8a509d1c14560ef4a97665
Usman Asif <pyroxicated@gmail.com>
2015-08-20 16:24:35 +0500

    Create Devise views and layout
    * Create customized views for required Devise actions
    * Create customized Devise layout
    * Update application layout to use head, navbar and footer partials
    * Update root paths for authenticated and unauthenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'rails/welcome#index', as: :root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
